e5e6704660903c9aef9ba79e3ca0f07cb6d7f2c0
einiyaselvie <einiyaselvielavarasan@gmail.com>
2025-07-16 18:19:34 +0530

    ADD increment and decrement

---
+++ js/incrementand decrement.js
@@ -0,0 +1,24 @@
+//Increment (++) and Decrement (--) Operators in JavaScript
+
+let a = 5;
+a++;
+console.log(a);
+
+let b = 10;
+b--;
+console.log(b);
+
+//postfix increament a++
+//prefixincrement ++a
+
+let x=3;
+const y=x++;
+console.log(x,y);
+
+let i = 3;
+const j=++i;
+console.log(i,j);
+
+
+
+
