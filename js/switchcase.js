056d28baa9e79bf68505774d71b2e3ab2f2d64be
einiyaselvie <einiyaselvielavarasan@gmail.com>
2025-07-17 17:41:49 +0530

    Switch case

---
+++ js/switchcase.js
@@ -0,0 +1,30 @@
+//switch case statement
+
+/* 
+switch(choice){
+case choice:
+    -------
+    break;
+    
+default:
+    -------
+    break;
+}
+*/
+
+let num = 0;
+
+switch(num){
+    case 1:
+        console.log("You have selected option 1");
+        break;
+    case 2:
+        console.log("You have selected option 2");
+        break;      
+    case 3:
+        console.log("You have selected option 3");
+        break;
+    default:
+        console.log("You have selected an invalid option");
+        break;
+}
