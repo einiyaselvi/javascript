674a1a712fac5d9095fb9acec67e3075b8ec5bfb
einiyaselvie <einiyaselvielavarasan@gmail.com>
2025-07-17 17:16:56 +0530

    add ifelseif

---
+++ js/if.js
@@ -0,0 +1,14 @@
+/* 
+if(condition){
+//statement
+}
+*/
+
+let age=prompt("Enter your age: ");
+if(age>=18){
+    console.log("you are eligible");
+}else{
+    console.log("you are not eligible");
+}
+
+  
---
+++ js/ifelseif.js
@@ -0,0 +1,21 @@
+//if- else if statement
+/*
+if(condition){
+    //statement
+}else if(condition){
+    //statement
+}else{
+    //statement
+}*/
+
+
+//example - 01
+
+let number = 0;
+if(number < 0){
+    console.log(number+"is negative number");
+}else if(number > 0){
+    console.log(number+" is positive number");
+}else{
+    console.log("zero");
+}
