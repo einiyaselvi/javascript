25dc238e051627a071fa3ff5a977e3f0a6279d51
einiyaselvie <einiyaselvielavarasan@gmail.com>
2025-07-20 11:12:31 +0530

    for loop

---
+++ js/forloop.js
@@ -0,0 +1,14 @@
+/* 
+for(intialization; condition; incre/decre){
+    //statement}
+*/
+
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+
+let arr=[];
+for(let i = 0; i < 100; i+=3){
+    arr.push(i);
+}
+console.log(arr);
